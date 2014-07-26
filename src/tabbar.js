0318d346dee77151447d7aea7886cff3db9237cf
dotch <cweiss@mozilla.com>
2014-07-26 12:30:11 -0700

    Update 2014-07-26T19:30:11.157Z

--- src/tabbar.js
+++ src/tabbar.js
@@ -1,5 +1,9 @@
 (function () {
 
+  var currentScript = document._currentScript || document.currentScript;
+
+
+
   function delegate(selector, handler) {
     return function(e) {
       var target = e.target;
@@ -17,20 +21,58 @@
   }
 
   function _selectTab(tabEl) {
-    var activeTab = tabEl.parentNode.querySelectorAll('brick-tabbar-tab[selected]');
+    var tabbar = tabEl.parentNode;
+    var indicator = tabbar.selectedIndicator;
+    var activeTab = tabbar.querySelectorAll('brick-tabbar-tab[selected]');
     for (var i = 0; i < activeTab.length; i++) {
       activeTab[i].removeAttribute('selected');
     }
     tabEl.setAttribute('selected', true);
+
+    var tabs = tabbar.tabs;
+    var index = tabs.indexOf(tabEl);
+    indicator.style.transform = "translateX(" + 100 * index + "%)";
+
   }
 
   var BrickTabbarElementPrototype = Object.create(HTMLElement.prototype);
 
-  BrickTabbarElementPrototype.attachedCallback = function () {
+  BrickTabbarElementPrototype.createdCallback = function () {
+
     var self = this;
-    self.selectHandler = delegate("brick-tabbar-tab", function(){ _selectTab(this); });
-    delegate(self,"select","brick-tabbar-tab", self.selectHandler);
-    delegate(self,"click","brick-tabbar-tab", self.selectHandler);
+    var importDoc = currentScript.ownerDocument;
+
+    var template = importDoc.querySelector("#brick-tabbar-template");
+
+    // fix styling for polyfill
+    if (Platform.ShadowCSS) {
+      var style = template.content.querySelector("style");
+      console.log(style);
+      var cssText = Platform.ShadowCSS.shimCssText(style.textContent, 'brick-tabbar');
+      console.log(cssText);
+      Platform.ShadowCSS.addCssToDocument(cssText);
+      style.remove();
+    }
+
+    // create shadowRoot and append template to it.
+    var shadowRoot = self.createShadowRoot();
+    shadowRoot.appendChild(template.content.cloneNode(true));
+
+    self.selectedIndicator = shadowRoot.querySelector(".selected-indicator");
+    self.selectedIndicator.style.width = 100 / self.tabs.length + "%";
+
+    self.selectHandler = delegate("brick-tabbar-tab", function(){
+      _selectTab(this);
+    });
+
+    self.addEventListener("click", self.selectHandler);
+    self.addEventListener("select", self.selectHandler);
+
+  };
+
+  BrickTabbarElementPrototype.detachedCallback = function () {
+    this.removeEventListener("click", this.selectHandler);
+    this.removeEventListener("select", this.selectHandler);
   };
 
   Object.defineProperties(BrickTabbarElementPrototype, {
