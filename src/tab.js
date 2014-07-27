9447eb4c4c7d924db0b19226fc2aad813b4c5596
dotch <cweiss@mozilla.com>
2014-07-27 00:50:29 -0700

    cleanup

--- src/tab.js
+++ src/tab.js
@@ -1,7 +1,7 @@
 (function () {
 
-  function _onTapbarTabClick (tabEl) {
-    if (tabEl.parentNode.nodeName.toLowerCase() === "brick-tabbar") {
+  function _onTapbarTabClick(tabEl) {
+    if (tabEl.parentNode.nodeName.toLowerCase() === 'brick-tabbar') {
       var targetEvent = tabEl.targetEvent;
       var target = tabEl.targetElement;
       target.dispatchEvent(new CustomEvent(targetEvent, {'bubbles': true}));
@@ -10,42 +10,43 @@
 
   var BrickTabbarTabElementPrototype = Object.create(HTMLElement.prototype);
 
-  BrickTabbarTabElementPrototype.attachedCallback = function () {
-    var self = this;
-    self.addEventListener("click", function(e){
+  BrickTabbarTabElementPrototype.attachedCallback = function() {
+    this.addEventListener('click', function(e) {
       var tabEl = e.currentTarget;
       _onTapbarTabClick(tabEl);
     });
-    self.addEventListener("select", function(e){
+    this.addEventListener('select', function(e) {
       var tabEl = e.currentTarget;
       _onTapbarTabClick(tabEl);
     });
   };
 
   BrickTabbarTabElementPrototype.select = function() {
-    this.dispatchEvent(new CustomEvent('select',{'bubbles': true}));
+    this.dispatchEvent(new CustomEvent('select', {
+      'bubbles': true
+    }));
   };
 
   Object.defineProperties(BrickTabbarTabElementPrototype, {
     'target': {
-      get: function () {
-        return this.getAttribute("target");
+      get: function() {
+        return this.getAttribute('target');
       },
-      set: function (newVal) {
+      set: function(newVal) {
         this.setAttribute('target', newVal);
       }
     },
     'targetEvent': {
-      get: function () {
-        if (this.hasAttribute("target-event")) {
-          return this.getAttribute("target-event");
-        } else if (this.parentNode.nodeName.toLowerCase() === "brick-tabbar") {
+      get: function() {
+        if (this.hasAttribute('target-event')) {
+          return this.getAttribute('target-event');
+        } else if (this.parentNode.nodeName.toLowerCase() === 'brick-tabbar') {
           return this.parentNode.targetEvent;
         } else {
-          throw "tabbar-tab is missing event to fire";
+          throw 'tabbar-tab is missing event to fire';
         }
       },
-      set: function (newVal) {
+      set: function(newVal) {
         this.setAttribute('target-event', newVal);
       }
     },
@@ -59,7 +60,7 @@
       },
       set: function(newVal) {
         this.overrideElement = newVal;
-        this.removeAttribute("target");
+        this.removeAttribute('target');
       }
     }
   });
--- src/tabbar.js
+++ src/tabbar.js
@@ -1,8 +1,8 @@
+/* global Platform */
+
 (function () {
 
   var currentScript = document._currentScript || document.currentScript;
-
-
 
   function delegate(selector, handler) {
     return function(e) {
@@ -22,71 +22,65 @@
 
   function _selectTab(tabEl) {
     var tabbar = tabEl.parentNode;
-    var indicator = tabbar.selectedIndicator;
     var activeTab = tabbar.querySelectorAll('brick-tabbar-tab[selected]');
     for (var i = 0; i < activeTab.length; i++) {
       activeTab[i].removeAttribute('selected');
     }
     tabEl.setAttribute('selected', true);
 
-    var tabs = tabbar.tabs;
-    var index = tabs.indexOf(tabEl);
-    indicator.style.transform = "translateX(" + 100 * index + "%)";
-
+    // move the indicator
+    var index = tabbar.tabs.indexOf(tabEl);
+    var indicator = tabbar.selectedIndicator;
+    indicator.style.transform = 'translateX(' + 100 * index + '%)';
   }
 
   var BrickTabbarElementPrototype = Object.create(HTMLElement.prototype);
 
-  BrickTabbarElementPrototype.createdCallback = function () {
+  BrickTabbarElementPrototype.createdCallback = function() {
 
-    var self = this;
     var importDoc = currentScript.ownerDocument;
-
-    var template = importDoc.querySelector("#brick-tabbar-template");
+    var template = importDoc.querySelector('#brick-tabbar-template');
 
     // fix styling for polyfill
     if (Platform.ShadowCSS) {
-      var style = template.content.querySelector("style");
-      console.log(style);
-      var cssText = Platform.ShadowCSS.shimCssText(style.textContent, 'brick-tabbar');
-      console.log(cssText);
+      var style = template.content.querySelector('style');
+      var cssText = Platform.ShadowCSS.shimStyle(style, 'brick-tabbar');
       Platform.ShadowCSS.addCssToDocument(cssText);
       style.remove();
     }
 
     // create shadowRoot and append template to it.
-    var shadowRoot = self.createShadowRoot();
+    var shadowRoot = this.createShadowRoot();
     shadowRoot.appendChild(template.content.cloneNode(true));
 
-    self.selectedIndicator = shadowRoot.querySelector(".selected-indicator");
-    self.selectedIndicator.style.width = 100 / self.tabs.length + "%";
+    this.selectedIndicator = shadowRoot.querySelector('.selected-indicator');
+    this.selectedIndicator.style.width = 100 / this.tabs.length + '%';
 
-    self.selectHandler = delegate("brick-tabbar-tab", function(){
+    this.selectHandler = delegate('brick-tabbar-tab', function(){
       _selectTab(this);
     });
 
-    self.addEventListener("click", self.selectHandler);
-    self.addEventListener("select", self.selectHandler);
-
+    this.addEventListener('click', this.selectHandler);
+    this.addEventListener('select', this.selectHandler);
   };
 
-  BrickTabbarElementPrototype.detachedCallback = function () {
-    this.removeEventListener("click", this.selectHandler);
-    this.removeEventListener("select", this.selectHandler);
+  BrickTabbarElementPrototype.detachedCallback = function() {
+    this.removeEventListener('click', this.selectHandler);
+    this.removeEventListener('select', this.selectHandler);
   };
 
   Object.defineProperties(BrickTabbarElementPrototype, {
     'targetEvent': {
-      get: function () {
-        return this.getAttribute("target-event") || "reveal";
+      get: function() {
+        return this.getAttribute('target-event') || 'reveal';
       },
-      set: function (newVal) {
+      set: function(newVal) {
         this.setAttribute('target-event', newVal);
       }
     },
     'tabs': {
       get: function() {
-        var tabList = this.querySelectorAll("brick-tabbar-tab");
+        var tabList = this.querySelectorAll('brick-tabbar-tab');
         return Array.prototype.slice.call(tabList);
       }
     }
